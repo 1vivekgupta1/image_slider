0524f37755e054b733ca02490d066bda4fd3a278
Vivek <1773379618+1vivekgupta1@users.noreply.github.com>
2025-03-08 12:55:35 +0530

    completed

--- script.js
+++ script.js
@@ -1,64 +1,90 @@
+const slider = document.querySelector(".slider");
 const slides = document.querySelectorAll(".slide");
-const slider = document.querySelectorAll(".slider");
-const nextNav = document.querySelector('.next')
-const prevNav = document.querySelector('.prev')
-
-
-slides.forEach((slide, index) => {
-  slide.style.transform = `translateX(${index * 100}%)`;
-});
+const nextNav = document.querySelector(".next");
+const prevNav = document.querySelector(".prev");
+const dotsNav = document.querySelector(".dots");
 
 let currentIndex = 0;
 let intervalId;
 
-// stop and slider when pointer is over the slides
-// slider.addEventListener("mouseenter", stopSlider);
+slides.forEach((slide, index) => {
+  // set the initial position of each slide
+  slide.style.transform = `translateX(${index * 100}%)`;
 
-//start the slider when pointer leaves the slides
-// slider.addEventListener("mouseleave", startSlider);
+  // create the dot nav buttons
+  const btn = document.createElement("button");
+  btn.classList.add("dot");
+  btn.classList.toggle("active", index === currentIndex);
+
+  // handle dot nav click event
+  btn.addEventListener("click", () => {
+    setCurrentSlide(index);
+    stopSlider();
+  });
+
+  dotsNav.appendChild(btn);
+});
+
+// start the slider when the page is loaded
+startSlider();
+
+// stop the slider when pointer is over the slides
+slider.addEventListener("mouseenter", stopSlider);
+
+// start the slider when pointer leaves the slides
+slider.addEventListener("mouseleave", startSlider);
 
 // handle next nav click event
-nextNav.addEventListener("click",() => {
-    showNextSlide();
-    stopSlider();
-})
+nextNav.addEventListener("click", () => {
+  showNextSlide();
+  stopSlider();
+});
+
 // handle previous nav click event
-prevNav.addEventListener("click",() => {
-    showPrevSlide();
-    stopSlider();
-})
+prevNav.addEventListener("click", () => {
+  showPreviousSlide();
+  stopSlider();
+});
 
 function startSlider() {
-  intervalId = setInterval(() => {
-    
-  }, 2000); //2secs
+  intervalId = setInterval(showNextSlide, 10000); // 10s
 }
 
 function stopSlider() {
   clearInterval(intervalId);
 }
 
-function showNextSlide(){
-    currentIndex = (currentIndex + 1) % slides.length;
+function showNextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
 
-    slides.forEach((slide, index) => {
-      const offset = (index - currentIndex) * 100;
-      slide.style.transform = `translateX(${offset}%)`;
-    });
+  updateSlides();
+  updateDots();
 }
 
-function showPrevSlide(){
-    currentIndex = (currentIndex - 1 + slides.length ) % slides.length;
+function showPreviousSlide() {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
 
-    slides.forEach((slide, index) => {
-      const offset = (index - currentIndex) * 100;
-      slide.style.transform = `translateX(${offset}%)`;
-    });
+  updateSlides();
+  updateDots();
 }
 
-function updateSlides(){
-    slides.forEach((slide, index) => {
-        const offset = (index - currentIndex) * 100;
-        slide.style.transform = `translateX(${offset}%)`;
-      });
+function setCurrentSlide(index) {
+  currentIndex = index;
+
+  updateSlides();
+  updateDots();
+}
+
+function updateSlides() {
+  slides.forEach((slide, index) => {
+    const offset = (index - currentIndex) * 100;
+    slide.style.transform = `translateX(${offset}%)`;
+
+    slide.classList.toggle("active", index === currentIndex);
+  });
+}
+
+function updateDots() {
+  const dots = document.querySelectorAll(".dot");
+  [...dots].map((dot, i) => dot.classList.toggle("active", i === currentIndex));
 }
